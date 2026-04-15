ed0a461d4d06b0775433e3fa15ed00de3fd09cc3
Sharon Castor <sharoncastor07@gmail.com>
2026-04-14 20:37:44 -0600

    Registro

---
+++ ProyectoVeterinaria/Views/RegistroView.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectoVeterinaria.Views
+{
+    /// <summary>
+    /// Lógica de interacción para RegistroView.xaml
+    /// </summary>
+    public partial class RegistroView : Window
+    {
+        public RegistroView()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
